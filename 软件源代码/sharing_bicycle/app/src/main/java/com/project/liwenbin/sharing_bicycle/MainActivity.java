7d5497cceec2f0b0b595e94479815728cf64a46f
黎文彬 <13557741335@163.com>
2017-03-25 23:06:01 +0800

    submit code

---
+++ 软件源代码/sharing_bicycle/app/src/main/java/com/project/liwenbin/sharing_bicycle/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.liwenbin.sharing_bicycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
